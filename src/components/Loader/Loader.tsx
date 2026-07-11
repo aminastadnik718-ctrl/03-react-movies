7cbb5a2e700eb267749fac7af63d505ee6207e6d
amina1108 <aminastadnik963@gmail.com>
2026-07-11 10:35:36 +0200

    finale section loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -5,9 +5,10 @@ import React from 'react'
 
 export const Loader: React.FC = () => {
     return (
- <p className={css.text}>Loading movies, please wait...</p>
-
+        <div className={css.loaderContainer} role="alert" aria-busy="true">
+            <div className={css.spinner}></div>
+            <p className={css.text}>Loading movies, please wait...</p>
+        </div>
     )
-            } 
-
+}
 
